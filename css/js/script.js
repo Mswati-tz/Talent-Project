04e25f9421b60f248e8e1c58540bac0a22e303b6
Mswati-tz <132760726+Mswati-tz@users.noreply.github.com>
2025-02-26 01:51:25 -0800

    Add files via upload

---
+++ css/js/script.js
@@ -0,0 +1,15 @@
+function view1(){
+    var click1 = document.getElementById("first");
+    var click2 = document.getElementById("second");
+    var click3 = document.getElementById("third");
+
+           if(click1.style.display ==="none"){
+               click1.style.display="block";
+               click2.style.display="none";
+               click3.style.display="none";
+               }else{
+                click1.style.display="none";
+
+        }
+        
+   } 
